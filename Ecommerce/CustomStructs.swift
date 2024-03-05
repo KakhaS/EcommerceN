9dec4488b94531b634c08603edd38c59b471279e
Kakha <Kakha.sepashvili@gmail.com>
2024-03-05 21:34:05 +0400

    First Changes

---
+++ Ecommerce/CustomStructs.swift
@@ -0,0 +1,18 @@
+//
+//  CustomStructs.swift
+//  EcommerceTests
+//
+//  Created by Kakha on 05.03.24.
+//
+
+import SwiftUI
+
+struct CustomStructs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomStructs()
+}
